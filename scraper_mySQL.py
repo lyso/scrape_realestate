8d25e33b589cada78736d86bd7253220960e9f17
lyso <lysomail@gmail.com>
2017-08-25 22:34:37 +1000

    Override Print function: writing to log file.

--- scraper_mySQL.py
+++ scraper_mySQL.py
@@ -2,10 +2,12 @@
 # MySQL
 from __future__ import print_function
 
-import requests
-from MySQL_connector import db_connector
 import datetime
 import time
+import zlib
+from MySQL_connector import db_connector
+
+import requests
 from bs4 import BeautifulSoup, element
 
 #
@@ -13,27 +15,51 @@ from bs4 import BeautifulSoup, element
 #     return db_connector('realestate_scraper')
 # db_connector = this_db_connector
 
-sql_create_tbl_html_text = "create table if not exists tbl_html_text(" \
-                          "hash_id INT NOT NULL, " \
-                          "html_text MEDIUMTEXT NOT NULL, " \
-                          "create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " \
-                          "url TEXT NOT NULL, " \
-                          "ad_text VARCHAR(255), " \
-                          "last_seen_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " \
-                          "PRIMARY KEY(hash_id) )"
-
-sql_create_tbl_scrap_log = "create table if not exists tbl_scrap_log(" \
+sql_create_tbl_html_text = "CREATE TABLE IF NOT EXISTS tbl_html_text(" \
+                           "hash_id INT NOT NULL, " \
+                           "html_text BLOB NOT NULL, " \
+                           "create_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " \
+                           "url TEXT NOT NULL, " \
+                           "ad_text VARCHAR(255), " \
+                           "last_seen_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " \
+                           "PRIMARY KEY(hash_id) )"
+sql_create_tbl_scrap_log = "CREATE TABLE IF NOT EXISTS tbl_scrap_log(" \
                            "scrap_id VARCHAR(255)," \
                            "note VARCHAR(255)," \
                            "timestamp TIMESTAMP NOT NULL DEFAULT current_timestamp);"
 
 
+def my_print_decorator(func):
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+    log_file_name = "./data/query_log_%s.txt" % now
+    last_print_end = ["\n"]
+    PRINT_LOG_FILE = lambda: open(log_file_name, "a")
+    with open(log_file_name, "w") as f:
+        f.write(str(datetime.datetime.now()) + "--> Start Application\n")
+
+    def print_and_log(*args,  **kwargs):
+        kwargs_copy = kwargs.copy()
+        end = kwargs_copy.pop("end", "\n")
+        sep = kwargs_copy.pop("sep", "   ")
+        content = sep.join([str(arg) for arg in args]) + end
+        if last_print_end[0] == "\n":
+            content = str(datetime.datetime.now()) + "-->" + content
+        last_print_end[0] = end
+        with PRINT_LOG_FILE() as f:
+            f.write(content)
+        return func(*args, **kwargs)
+
+    return print_and_log
+
+print = my_print_decorator(print)
+
+
 class BaseScraper(object):
-    db = 'realestate_scraper'
+    db = 'scraper_dumps'
 
     def __init__(self):
         self.domain = "www.realestate.com.au"
-        self.url_template = "http://www.realestate.com.au/buy/in-%s/list-%s?includeSurrounding=false"
+        self.url_template = "http://www.realestate.com.au/buy/in-%s/list-%s?activeSort=list-date&includeSurrounding=false"
         self.page_num = 1
         self.postcode = 2077
         self.soup = None
@@ -71,8 +97,6 @@ class BaseScraper(object):
 
         return str(scrap_id) in self.scrap_log
 
-
-
     @property
     def url(self):
         return self.url_template % (self.postcode, self.page_num)
@@ -94,7 +118,7 @@ class BaseScraper(object):
         else:
             sql = "INSERT INTO tbl_html_text (hash_id, html_text, url, ad_text)" \
                   " VALUES (%s, %s, %s, %s)"
-            val = (hash_id, html_text, self.url, ad_text)
+            val = (hash_id, zlib.compress(html_text.encode('utf8'), 9), self.url, ad_text)
             self._cur.execute(sql, val)
             self._conn.commit()
             return True
@@ -147,18 +171,20 @@ if __name__ == "__main__":
         postcode = postcodes[pn]
         scraper.postcode = postcode
         scraper.page_num = 1
+        saved_tags_this_postcode = 0
+
         print('\n', "*" * 40, end='')
         print("POSTCODE:", postcode, "(%d/%d)" % (pn, len(postcodes)), "*" * 40)
+
         if scraper.has_scraped(postcode):
             print("------------skipping this postcode.-------------")
             continue
 
         time.sleep(2)
-        saved_tags_this_postcode = 0
 
         while True:
             try:
-                print('\n', "-"*80)
+                print('\n', "-" * 80)
                 print(datetime.datetime.now())
                 print(scraper.url)
                 print("loading web page...", end='')
@@ -168,10 +194,15 @@ if __name__ == "__main__":
                 toc = time.time()
                 print("(%f seconds used)" % (toc - tic))
 
-                if len(tags) == 0:
+                if len(tags) == 0:  # when no property found on this page, go for next postcode
                     print("no property found.")
+                    found_tag_qty = len(tags) + 20 * (scraper.page_num - 1)
+                    note_text = "Postcode=%s, Scraped Pages=%d, Ad Found =%d, Saved=%d" % \
+                                (str(postcode), scraper.page_num, found_tag_qty, saved_tags_this_postcode)
+                    scraper.log_scrap(postcode, note_text)
                     break
 
+                # found at list one properties, save to db
                 assert isinstance(tags[0], element.Tag)
                 print("%d properties are found, saving to database..." % len(tags))
                 i = 0
@@ -182,20 +213,12 @@ if __name__ == "__main__":
                 tic = time.time()
                 print("%d are saved. (%f seconds used)" % (i, (tic - toc)))
 
-                if len(tags) < 20:
-                    found_tag_qty = len(tags) + 20 * (scraper.page_num-1)
+                if len(tags) < 20:  # when less than 20 properties found on this page, go for next postcode
+                    found_tag_qty = len(tags) + 20 * (scraper.page_num - 1)
                     note_text = "Postcode=%s, Scraped Pages=%d, Ad Found =%d, Saved=%d" % \
                                 (str(postcode), scraper.page_num, found_tag_qty, saved_tags_this_postcode)
                     scraper.log_scrap(postcode, note_text)
                     break
-                # if i == 0:
-                #     scraper.page_num += step
-                #     step *= 2
-                #     last_saved_page = scraper.page_num
-                # else:
-                #     if step > 1:
-                #         step = 1
-                #         scraper.page_num = last_saved_page0
             except Exception as err:
                 print(err)
                 if err_happens:
@@ -211,8 +234,3 @@ if __name__ == "__main__":
                 scraper.page_num += 1
                 if scraper.page_num % 10 == 0:
                     time.sleep(2)
-
-
-
-
-
